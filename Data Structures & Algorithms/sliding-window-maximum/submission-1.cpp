2c6a56fca97660f5a336e5b918cddc278d9bf88a
Siddharth verma <siddharth26338@gmail.com>
2026-08-22 18:26:48 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+
+        for(int i =0 ; i<k; i++){
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+
+        for(int i = k ;i<nums.size() ; i++){
+            ans.push_back(nums[dq.front()]);
+            while(dq.size()>0 && dq.front() <= i-k ) dq.pop_front();
+
+            while(dq.size() > 0 && nums[dq.back()] <= nums[i]) dq.pop_back();
+            
+            dq.push_back(i);
+
+        }
+         ans.push_back(nums[dq.front()]);
+
+        return ans;
+    }
+};
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+//        vector<int> ans;
+//        deque<int> dq;
+
+//        for(int i=0; i<k ; i++){
+//         while(dq.size() > 0 && nums[dq.back()] <= nums[i])  dq.pop_back();
+       
+//        dq.push_back(i);
+//        }
+
+//        for (int i=k; i<nums.size(); i++){
+//         ans.push_back(nums[dq.front()]);
+
+//         while(dq.size()>0 && dq.front() <= i-k) dq.pop_front();
+
+//         while(dq.size() > 0 && nums[dq.back()] <= nums[i])  dq.pop_back();
+
+//         dq.push_back(i);
+
+//        }
+//        ans.push_back(nums[dq.front()]);
+//        return ans;
+//     }
+// };
