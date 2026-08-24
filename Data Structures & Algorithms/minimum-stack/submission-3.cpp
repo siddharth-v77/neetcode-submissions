6df0e3c111f36fbe30ee18142b46aaaa22438c9c
Siddharth verma <siddharth26338@gmail.com>
2026-08-24 11:43:10 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.cpp
@@ -0,0 +1,148 @@
+// class MinStack {
+// public:
+// stack<pair<int,int>> st;
+//     MinStack() {
+        
+//     }
+    
+//     void push(int val) {
+//         if(st.empty()) st.push({val,val});
+//         else {int minVal = min(val, st.top().second);
+//         st.push({val, minVal});}
+         
+//     }
+    
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//          return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second  ;  
+//     }
+// };
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(value);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
+
+
+//  class MinStack {
+// public:
+//     stack<long long int> st;
+//     long long int minVal;
+//     MinStack() {
+       
+//     }
+    
+//     void push(int val) {
+//         if(st.empty() ) 
+//         {st.push(val) ;
+//         minVal = val;}
+        
+//         else if (val < minVal){
+//             st.push((long long) 2 * val-minVal);
+//             minVal = val;
+//         } else st.push(val);
+//     }
+    
+//     void pop() {
+        
+//         if(st.top()<minVal){
+//             minVal = 2*minVal - st.top();
+
+//         }
+//         st.pop();
+//     }
+    
+//     int top() {
+//         if(st.top()<minVal){
+//             return minVal;
+//         }
+//         return st.top();
+//     }
+    
+//     int getMin() {
+//      return minVal;   
+//     }
+// } ;
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * MinStack* obj = new MinStack();
+//  * obj->push(value);
+//  * obj->pop();
+//  * int param_3 = obj->top();
+//  * int param_4 = obj->getMin();
+//  */
+
+
+
+
+
+
+
+
+ class MinStack {
+public:
+
+    stack<long long> st;
+    long long minval ;
+    
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push(val);
+            minval = val;
+        }
+
+        else if(val < minval){
+            st.push((long long) 2* val - minval);
+            minval = val;
+        }
+
+        else st.push(val);
+    }
+    
+    void pop() {
+        if(st.top() < minval){
+            minval = 2*minval - st.top();
+        }
+
+       st.pop();
+    }
+    
+    int top() {
+    if(st.top() < minval){
+       return minval; 
+    }
+    return st.top();
+
+    }
+    
+    int getMin() {
+       return minval; 
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(value);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
