2cb7f5f83f9cafe9f0bca6b8a85a1091259b8786
Siddharth verma <siddharth26338@gmail.com>
2026-08-30 11:55:49 +0530

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,double>> car;
+        for(int i = 0; i<position.size(); i++){
+            double time = (double) (target-position[i])/speed[i];
+            car.push_back({position[i],time});
+        }
+
+        sort(car.begin() , car.end() , greater<pair<int,double>>());
+
+        int count =0 ;
+        double prevtime =0;
+
+        for( auto i : car){
+            if(i.second > prevtime){
+                count++;
+                prevtime = i.second;
+            }
+
+        }
+        return count;
+
+    }
+};
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int carFleet(int target, vector<int>& position, vector<int>& speed) {
+//      int n = position.size();
+//      vector<pair<int,double>> cars;
+
+//      for(int i=0; i<n ; i++){
+//         double time = (double) (target-position[i])/speed[i];
+//         cars.push_back({position[i],time});
+//      }  
+
+//      sort(cars.begin(),cars.end() , greater<pair<int,double>>());
+//      int count =0 ;
+//      double prevtime=0;
+//     for( auto car: cars){
+//         if(car.second>prevtime) {count++;
+//         prevtime= car.second;
+// }
+//     }
+//         return count;
+//     }
+// };
