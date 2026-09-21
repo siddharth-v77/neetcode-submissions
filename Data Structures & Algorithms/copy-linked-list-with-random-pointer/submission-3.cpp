f0f8b0db98e234b1347d0d5b939076d473771244
Siddharth verma <siddharth26338@gmail.com>
2026-09-21 12:28:01 +0530

    Add: copy-linked-list-with-random-pointer - submission-3

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-3.cpp
@@ -0,0 +1,105 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(head == NULL){
+            return head;
+        }
+
+        Node* newhead = new Node(head->val);
+        Node* oldtmp = head->next;
+        Node* newtmp = newhead;
+
+        unordered_map<Node*,Node*> mp ;
+        mp[head] = newhead;
+        while(oldtmp != NULL){
+            Node* copynode = new Node(oldtmp->val);
+            mp[oldtmp] = copynode;
+            newtmp->next = copynode;
+
+            oldtmp = oldtmp->next;
+            newtmp = newtmp->next; 
+        }
+        
+         newtmp = newhead;
+         oldtmp = head;
+        while(oldtmp != NULL){
+            newtmp->random = mp[oldtmp->random];
+
+            oldtmp = oldtmp->next;
+            newtmp = newtmp->next;
+        }
+        return newhead;
+    }
+};
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+// class Solution {
+// public:
+//     Node* copyRandomList(Node* head) {
+//        if( head == NULL){
+//         return NULL;
+//        } 
+
+//        unordered_map<Node*,Node*>m;
+
+//        Node* newhead = new Node(head->val);
+//        Node* oldTemp = head->next;
+//        Node* newTemp = newhead;
+
+//         m[head] = newhead;
+//        while(oldTemp != NULL){
+//         Node* copynode = new Node(oldTemp->val);
+//         m[oldTemp] = copynode;
+//         newTemp->next = copynode;
+
+//         oldTemp = oldTemp->next;
+//         newTemp = newTemp->next;
+
+//        }
+
+//        oldTemp = head;
+//         newTemp = newhead;
+        
+//         while(oldTemp != NULL){
+//             newTemp->random = m[oldTemp->random];
+
+//              oldTemp = oldTemp->next;
+//             newTemp = newTemp->next;
+
+//         }
+
+//         return newhead;
+//     }
+// };
