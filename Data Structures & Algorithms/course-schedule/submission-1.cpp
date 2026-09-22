77fd851ffe7062c99f62893fe53a8f56ba33d3c7
Siddharth verma <siddharth26338@gmail.com>
2026-09-22 12:17:39 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+
+    bool iscycledfs(int src , vector<bool> &vis , vector<bool> &recPath ,vector<vector<int>>& edges ){
+        vis[src] = true;
+        recPath[src] = true;
+
+        for(int i = 0 ; i < edges.size() ; i++){
+            int u = edges[i][1];
+            int v = edges[i][0];
+            if(u == src){
+                if(!vis[v]){
+                    if( iscycledfs(v , vis , recPath , edges )){
+                        return true;
+                    }
+                }
+                else if(recPath[v]) return true; 
+            }
+        }
+        recPath[src] = false;
+        return false;
+    }
+
+    bool canFinish(int n, vector<vector<int>>& edges) {
+     vector<bool> vis(n,false)   ;
+     vector<bool> recPath(n,false);   
+
+     for(int i = 0 ; i < n ; i++){
+      if(!vis[i]){
+
+       if( iscycledfs( i , vis , recPath , edges)) {
+        return false;}
+
+      }  
+     }
+
+     return true;
+    }
+};
