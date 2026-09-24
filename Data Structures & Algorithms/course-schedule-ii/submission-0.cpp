aff6d763b71f706020640b2e68b3b41ed5d20308
Siddharth verma <siddharth26338@gmail.com>
2026-09-24 08:10:01 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+
+    bool iscycledfs(int src , vector<bool> &vis ,vector<bool> &recPath ,vector<vector<int>>& edges ){
+        vis[src] = true;
+        recPath[src] = true;
+
+        for( int i =0 ; i < edges.size() ; i++ ){
+            int u = edges[i][1];
+            int v  = edges[i][0];
+
+            if(u == src){
+                if(!vis[v]){
+                    if(iscycledfs(v , vis, recPath , edges)){
+                        return true;
+                    }
+                }
+                else if ( recPath[v]) return true;
+            }
+        }
+
+        recPath[src] = false;
+        return false;
+    }
+
+    void topoOrder(int src ,vector<bool> &vis ,stack<int> &s ,vector<vector<int>>& edges ){
+        vis[src] = true;
+
+        for( int i =0 ; i < edges.size() ; i++){
+            int u = edges[i][1];
+            int v = edges[i][0];
+            if( u == src){
+                if(!vis[v]){
+                    topoOrder(v , vis, s ,edges);
+                }
+            }
+        }
+        s.push(src);
+    }
+
+    vector<int> findOrder(int n, vector<vector<int>>& edges) {
+        vector<bool> vis(n,false);
+        vector<bool> recPath(n,false);
+        vector<int> ans;
+
+        for(int i =0 ; i < n ; i++){
+            if(!vis[i]){
+                if(iscycledfs(i, vis , recPath , edges)){
+                    return ans;
+                }
+            }
+        }
+
+        stack<int> s ;
+        vis.assign(n,false);
+
+        for(int i =0 ; i < n ; i++){
+            if(!vis[i]){
+                topoOrder(i, vis , s,edges );
+            }
+        }
+
+        while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+        }
+        return ans;
+    }
+};
